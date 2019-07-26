3f67326ff226eeff0f016144fbd74b3984b2adbd
xintru <52363285+xintru@users.noreply.github.com>
2019-07-27 06:37:22 +0700

    Bugfix and general refactor.
    Fixed bug when menu wasn't opening on mobile phones and tablets.

--- mapScript.js
+++ mapScript.js
@@ -1,9 +1,9 @@
 function initMap() {
-  // The location of Uluru
+  // The location of Novosibirsk
   var novosibirsk = {lat: 55.026424, lng: 82.883447};
-  // The map, centered at Uluru
+  // The map, centered at Novosibirsk
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: novosibirsk});
-  // The marker, positioned at Uluru
+      document.getElementById('map'), {zoom: 12, center: novosibirsk});
+  // The marker, positioned at Novosibirsk
   var marker = new google.maps.Marker({position: novosibirsk, map: map});
 }
--- menu.js
+++ menu.js
@@ -1,4 +1,17 @@
-$('#sliderButton').click(function() {
-    $('#nav').toggleClass('visible');
-    $('#sliderButton').toggleClass('reversed');
-});
+var navBlock = document.querySelector('.navigation');
+var sliderButton = document.querySelector('.slider-button');
+
+sliderButton.addEventListener('click', handleInteraction);
+sliderButton.addEventListener('touchstart', handleInteraction);
+
+function handleInteraction(evt) {
+    if (!navBlock.classList.contains('visible') && !sliderButton.classList.contains('reversed')) {
+        evt.preventDefault();
+        navBlock.classList.add('visible');
+        sliderButton.classList.add('reversed');
+    } else {
+        evt.preventDefault();
+        navBlock.classList.remove('visible');
+        sliderButton.classList.remove('reversed');
+    }
+}
